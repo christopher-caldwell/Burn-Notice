b919d68e68b1f645d61413857d3dae3d9e134d64
caldwell619 <caldwell8814@gmail.com>
2020-03-07 03:46:25 -0600

    adding mutation of login

--- api/src/lambdas/user/login/index.js
+++ api/src/lambdas/user/login/index.js
@@ -2,6 +2,7 @@ const Responder = require('simple-lambda-actions/dist/util/responseHandler')
 const { bodyParser } = require('simple-lambda-actions/dist/util/formatter')
 const generateToken = require('./lib/secretsManagerSetup')
 const findAndVerify = require('./lib/findAndVerify')
+const updateLogin = require('./lib/updateLoginTime')
 
 const corsUrl = process.env.CORS_URL
 
@@ -10,9 +11,15 @@ exports.handler = async event => {
 	try {
 		const { sap, password } = bodyParser(event.body)
 		const userProfile = await findAndVerify(sap, password)
-
+		const { id } = userProfile
 		const tokenParams = { id: userProfile.sap, role: userProfile.role }
-		const token = await	generateToken(tokenParams)
+
+		const [ token ] = await	Promise.all([
+			generateToken(tokenParams),
+			updateLogin(id)
+		])
+
+		userProfile.lastLoggedInAt = new Date()
 
 		return ResponseHandler.respond({ userProfile, token }, 200)
 	} catch(error){
