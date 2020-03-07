54c09562dabaafa58e9df6600df8d107df674114
caldwell619 <caldwell8814@gmail.com>
2020-03-07 10:33:18 -0600

    updating to allow queries

--- api/src/lambdas/graphql/index.js
+++ api/src/lambdas/graphql/index.js
@@ -1,7 +1,18 @@
 const { graphql } = require('graphql')
+const Responder = require('simple-lambda-actions/dist/util/responseHandler')
+const { bodyParser } = require('simple-lambda-actions/dist/util/formatter')
 const { schema, resolvers } = require('./lib')
 
+const corsUrl = process.env.CORS_URL
+
 exports.handler = async event => {
-	const result = await graphql(schema, event.body, resolvers)
-	return { statusCode: 200, body: JSON.stringify(result.data, null, 2) }
+	const ResponseHandler = new Responder(corsUrl, event.httpMethod)
+	const { query } = bodyParser(event.body)
+	const result = await graphql(schema, query, resolvers)
+	if(!result.errors){
+		return ResponseHandler.respond(result.data, 200)
+	} else {
+		console.log('error', result.errors[0])
+		return ResponseHandler.respond(result.errors[0].message, 500)
+	}
 }
