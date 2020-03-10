de53108507c682b0af44beb51b1be24af2ceeb51
caldwell619 <caldwell8814@gmail.com>
2020-03-09 23:05:08 -0500

    initial

---
+++ api/src/lambdas/user/home/index.js
@@ -0,0 +1,26 @@
+const Responder = require('simple-lambda-actions/dist/util/responseHandler')
+const queryUpdates = require('./lib/query')
+const shuffle = require('simple-array-shuffler')
+
+const corsUrl = process.env.CORS_URL
+
+const numOfVacanciesToReturn = 5
+
+exports.handler = async event => {
+	const ResponseHandler = new Responder(corsUrl, event.httpMethod)
+	try {
+		const { sap } = event.queryStringParameters
+		
+		const [ updates, vacancies ] = await Promise.all([
+			await queryUpdates(sap, 'update'),
+			await queryUpdates('vacancy')
+		])
+		const shuffledVacancies = shuffle(vacancies)
+		shuffledVacancies.splice(numOfVacanciesToReturn)
+		
+		return ResponseHandler.respond({ updates, vacancies }, 200)
+	} catch(error){
+		console.log('error', error)
+		return ResponseHandler.respond(error.message, error.statusCode || 500)
+	}
+}
