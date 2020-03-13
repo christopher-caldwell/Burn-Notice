da5cff467259017bc4362dac7959a4e8bfdb31e4
caldwell619 <caldwell8814@gmail.com>
2020-03-13 18:21:27 -0500

    hard coding non changing vals

--- api/src/lambdas/user/register/lib/putUser.js
+++ api/src/lambdas/user/register/lib/putUser.js
@@ -1,10 +1,10 @@
 const { genSalt, hash } = require('bcryptjs/dist/bcrypt.min')
 const { knexSnakeCaseMappers } = require('objection/lib/utils/identifierMapping')
 const knex = require('knex')({
-  client: process.env.DB_CLIENT,
+  client: 'pg',
   connection: {
     host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
+    port: 5432,
     database: process.env.DB_NAME,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD
