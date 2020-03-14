77699eb95a29e8f0df2b756399000b63e19829d2
caldwell619 <caldwell8814@gmail.com>
2020-03-14 00:09:52 -0500

    changing to username

--- api/src/lambdas/user/register/lib/putUser.js
+++ api/src/lambdas/user/register/lib/putUser.js
@@ -1,12 +1,13 @@
 const { genSalt, hash } = require('bcryptjs/dist/bcrypt.min')
 const { knexSnakeCaseMappers } = require('objection/lib/utils/identifierMapping')
+
 const knex = require('knex')({
   client: 'pg',
   connection: {
     host: process.env.DB_HOST,
     port: 5432,
     database: process.env.DB_NAME,
-    user: process.env.DB_USER,
+    user: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD
 	},
 	...knexSnakeCaseMappers()
