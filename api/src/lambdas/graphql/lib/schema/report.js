206da66875701cda7c3205e14504fbab4a6e6181
caldwell619 <caldwell8814@gmail.com>
2020-03-16 14:30:11 -0500

    adding input for creation

--- api/src/lambdas/graphql/lib/schema/report.js
+++ api/src/lambdas/graphql/lib/schema/report.js
@@ -2,6 +2,7 @@ const reportSchema = `
 	type Report {
 		id: ID
 		submitter: Account
+		fireStation: FireStation
 		typeOfIncident: String
 		createdAt: Date
 		timeDispatched: Date
@@ -14,15 +15,31 @@ const reportSchema = `
 		teamOneMembers: [Account]
 		teamTwoMembers: [Account]
 	}
+	input ReportInput {
+		submitter: Int
+		fireStation: Int
+		typeOfIncident: String
+		createdAt: Date
+		timeDispatched: Date
+		timeArrived: Date
+		wasExposedToChem: Boolean
+		wasFireRetardantPresent: Boolean
+		actionsOfPrimaryTeam: String
+		actionsOfSecondaryTeam: String
+		descriptionOfEvents: String
+		teamOneMembers: [Int]
+		teamTwoMembers: [Int]
+	}
 `
 
 const reportQuery = `
 	report(id: ID): Report
 	reports: [Report]
+	reportsByAccountId(id: ID): [Report]
 `
 
 const reportMutation = `
-	
+	createReport(params: ReportInput): Report
 `
 
 module.exports = { reportSchema, reportQuery, reportMutation }
